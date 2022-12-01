9241edccf194af5d495ca02f9744a869679838bd
jMampaey <82814594+jMampaey@users.noreply.github.com>
2022-12-01 10:36:50 +0100

    Update Program.cs

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace AdventOfCode2022
 {
@@ -6,7 +7,38 @@ namespace AdventOfCode2022
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program.PrintFile();
+        }
+
+        private static void PrintFile()
+        {
+            var lines = File.ReadAllLines("Day1/Puzzle1.txt");
+            var currentElf = 1;
+            int biggestElf = default;
+            var elfTotal = 0;
+            var biggestTotal = 0;
+
+            foreach(var line in lines)
+            {
+                if (line == "")
+                {
+                    Console.WriteLine(currentElf + " - " + elfTotal);
+                    if (elfTotal > biggestTotal)
+                    {
+                        biggestTotal = elfTotal;
+                        biggestElf = currentElf;
+                    }    
+
+                    currentElf++;
+                    elfTotal = 0;
+                    continue;
+                }
+                var value = Int32.Parse(line);
+                elfTotal += value;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(biggestElf + " - " + biggestTotal);
         }
     }
 }
