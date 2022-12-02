76657f6abc6d6fa9631bc5d03e68f20bedcfe6f8
jMampaey <82814594+jMampaey@users.noreply.github.com>
2022-12-02 18:23:12 +0100

    Update Program.cs

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -14,32 +14,46 @@ namespace AdventOfCode2022
 
         private static void PrintFile()
         {
-            var lines = File.ReadAllLines("Day1/Puzzle1.txt");
+            var lines = File.ReadAllLines("Day2/Puzzle1.txt");
 
-            var elves = new List<int>();
-            var elfTotal = 0;
+            var totalPoints = 0;
 
             foreach(var line in lines)
             {
-                if (line == "")
-                {
-                    elves.Add(elfTotal);
-                    elfTotal = 0;
-                    continue;
-                }
-                var value = Int32.Parse(line);
-                elfTotal += value;
+                var newLine = line.Replace("X", "A");
+                newLine = newLine.Replace("Y", "B");
+                newLine = newLine.Replace("Z", "C");
+
+                var points = 0;
+                var options = newLine.Split(" ");
+                Console.WriteLine(options[0] + " - " + options[1]);
+                points += GetPointsForOwnHand(options[1]);
+                points += GetPointsForResult(options);
+                totalPoints += points;
             }
 
-            var total = 0;
-            foreach (var elf in elves.OrderByDescending(e => e).Take(3))
-            {
-                total += elf;
-                Console.WriteLine(elf);
-            }
+            Console.WriteLine(totalPoints);
+        }
 
-            Console.WriteLine("\n" + total);
+        private static int GetPointsForResult(string[] options)
+        {
+            if ((options[0] == "A" && options[1] == "B") || (options[0] == "B" && options[1] == "C") || (options[0] == "C" && options[1] == "A"))
+                return 6;
+            if (options[0] == options[1])
+                return 3;
+            return 0;
+        }
 
+        private static int GetPointsForOwnHand(string value)
+        {
+            if (value == "A")
+                return 1;
+            if (value == "B")
+                return 2;
+            if (value == "C")
+                return 3;
+
+            return default;
         }
     }
 }
