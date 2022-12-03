76bfca9822ad095112ca591fcce0dccc25c87ead
jMampaey <82814594+jMampaey@users.noreply.github.com>
2022-12-03 15:58:15 +0100

    Update Program.cs

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -14,64 +14,42 @@ namespace AdventOfCode2022
 
         private static void PrintFile()
         {
-            var lines = File.ReadAllLines("Day2/Puzzle1.txt");
+            var lines = File.ReadAllLines("Day3/Puzzle1.txt");
 
-            var totalPoints = 0;
-
-            foreach(var line in lines)
+            var total = 0;
+            foreach (var line in lines)
             {
-                var points = 0;
-                var options = line.Split(" ");
-                points += GetPointsForOwnHand(options);
-                points += GetPointsForResult(options[1]);
-                totalPoints += points;
+                var firstString = line.Substring(0, line.Length / 2);
+                var secondString = line.Substring(line.Length / 2, line.Length / 2);
+
+                total += GetPriority(firstString, secondString);
             }
 
-            Console.WriteLine(totalPoints);
+            Console.WriteLine("\n" + total);
         }
 
-        private static int GetPointsForResult(string result)
+        private static int GetPriority(string firstString, string secondString)
         {
-            if (result == "X")
-                return 0;
-            if (result == "Y")
-                return 3;
-            return 6;
-        }
-
-        private static int GetPointsForOwnHand(string[] options)
-        {
-            if (options[1] == "X")
+            foreach (var item in firstString.ToCharArray())
             {
-                if (options[0] == "A")
-                    return 3;
-                if (options[0] == "B")
-                    return 1;
-                if (options[0] == "C")
-                    return 2;
-            }
-
-            if (options[1] == "Y")
-            {
-                if (options[0] == "A")
-                    return 1;
-                if (options[0] == "B")
-                    return 2;
-                if (options[0] == "C")
-                    return 3;
-            }
-
-            if (options[1] == "Z")
-            {
-                if (options[0] == "A")
-                    return 2;
-                if (options[0] == "B")
-                    return 3;
-                if (options[0] == "C")
-                    return 1;
+                if (secondString.ToCharArray().Contains(item))
+                    return GetValueForChar(item);
             }
 
             return default;
+        }
+
+        private static int GetValueForChar(char item)
+        {
+            var value = 0;
+            if (char.IsUpper(item))
+                value = item - 38;
+            else
+                value = item - 96;
+
+            Console.WriteLine(item + " - " + value);
+
+            return value;
         }
     }
 }
